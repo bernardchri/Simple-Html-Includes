4965266783253e2356e08402f6d53fb4c4f182df
Christophe Bernard <christophe.bernard@havasdigitalfactory.com>
2019-09-25 23:33:30 +0200

    FrontEnd | clean files

--- src/index.php
+++
@@ -1,8 +0,0 @@
-<?php include('header.php') ?>
-
-<!-- bloc question -->
-<?php include('bloc-question-desktop.php') ?>
-<?php include('bloc-question-mobile.php') ?>
-
-<!-- footer -->
-<?php include('footer.php') ?>
